454a47a49a981cc2a784ae90ff47d33a716620d7
Leopold Talirz <leopold.talirz@gmail.com>
2019-07-23 16:03:12 +0200

    backport changes from develop
     * update aiidalab metapackage
     * fix pythonpath issue in startup script

--- templates/start.sh
+++ templates/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source "{{ aiidalab_venv }}/bin/activate"
-export PYTHONPATH=$PYTHONPATH:{{ aiidalab_base_folder }}
+export PYTHONPATH=${PYTHONPATH}${PYTHONPATH:+:}{{ aiidalab_base_folder }}
 
 if jupyter notebook list | grep -q 'localhost'; then
     url=`jupyter notebook list | grep -m1 localhost | awk '{print $1}'`
